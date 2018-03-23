01674431b0aa857f1819777a2cdbd5536cc0a243
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2018-03-23 12:55:55 +0100

    added basic build script for an AppImage

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# build application directory
+ant image
+
+# replace jvm
+rm -rf antbuild/Cryptomator/runtime
+${JAVA_HOME}/bin/jlink \
+  --module-path ${JAVA_HOME}/jmods \
+  --compress 1 \
+  --no-header-files \
+  --strip-debug \
+  --no-man-pages \
+  --strip-native-commands \
+  --output antbuild/Cryptomator/runtime \
+  --add-modules java.base,java.logging,java.xml,java.sql,java.management,java.security.sasl,java.naming,java.datatransfer,java.security.jgss,java.rmi,java.scripting,java.prefs,java.desktop,jdk.incubator.httpclient,javafx.fxml,javafx.controls,jdk.incubator.httpclient \
+  --verbose
+
+# build AppDir
+mkdir -p Cryptomator.AppDir/usr/bin/
+mkdir -p Cryptomator.AppDir/opt/
+cp resources/appimage/cryptomator.png Cryptomator.AppDir
+cp resources/appimage/cryptomator.desktop Cryptomator.AppDir
+cp resources/appimage/cryptomator.sh Cryptomator.AppDir/usr/bin/
+cp -r antbuild/Cryptomator Cryptomator.AppDir/opt
+curl -L https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64 -o Cryptomator.AppDir/AppRun
+chmod +x Cryptomator.AppDir/AppRun
+chmod +x Cryptomator.AppDir/usr/bin/cryptomator.sh
+
+# build AppImage
+export ARCH=x86_64
+curl -L https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage -o appimagetool.AppImage
+chmod +x appimagetool.AppImage
+./appimagetool.AppImage Cryptomator.AppDir Cryptomator.AppImage
---
+++ resources/appimage/cryptomator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+HERE=$(dirname $(readlink -f "${0}"))
+${HERE}/../../opt/Cryptomator/Cryptomator
