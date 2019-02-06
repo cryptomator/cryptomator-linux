205c47ed958287b51fcf215aa989b5a99947dc8d
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2019-02-06 16:56:07 +0100

    Using snapshot of jep 343 packager with OpenJDK 11
    Requires buildkit.zip to contain all .jar files (including openfjx)

--- build.sh
+++ build.sh
@@ -1,56 +1,53 @@
 #!/bin/bash
-BUILD_VERSION=$1
+BUILD_VERSION=${1:-continuous}
 
-# build application directory
-ant \
-  -Dantbuild.logback.configurationFile=logback.xml \
-  -Dantbuild.cryptomator.settingsPath=~/.Cryptomator/settings.json \
-  -Dantbuild.cryptomator.ipcPortPath=~/.Cryptomator/ipcPort.bin \
-  -Dantbuild.cryptomator.keychainPath= \
-  -Dantbuild.dropinResourcesRoot= \
-  image
+# check preconditions
+if [ -z "${JAVA_HOME}" ]; then echo "JAVA_HOME not set. Run using JAVA_HOME=/path/to/java/ ./build.sh"; exit 1; fi
+if [ ! -x ./tools/packager/jpackager ]; then echo "../tools/packager/jpackager not executable."; exit 1; fi
+if [ ! -x ./tools/appimagekit/appimagetool-x86_64.AppImage ]; then echo "./tools/appimagekit/appimagetool-x86_64.AppImage not executable."; exit 1; fi
 
-# replace jvm
-rm -rf antbuild/Cryptomator/runtime
-${JAVA_HOME}/bin/jlink \
-  --module-path ${JAVA_HOME}/jmods \
-  --compress 1 \
-  --no-header-files \
-  --strip-debug \
-  --no-man-pages \
-  --strip-native-commands \
-  --output antbuild/Cryptomator/runtime \
-  --add-modules java.base,java.logging,java.xml,java.sql,java.management,java.security.sasl,java.naming,java.datatransfer,java.security.jgss,java.rmi,java.scripting,java.prefs,java.desktop,javafx.fxml,javafx.controls \
-  --verbose
+# create .app
+./tools/packager/jpackager create-image \
+    --verbose \
+    --echo-mode \
+    --input libs \
+    --main-jar launcher-${BUILD_VERSION}.jar  \
+    --class org.cryptomator.launcher.Cryptomator \
+    --jvm-args "-Dlogback.configurationFile=\"logback.xml\"" \
+    --jvm-args "-Dcryptomator.settingsPath=\"~/.Cryptomator/settings.json\"" \
+    --jvm-args "-Dcryptomator.ipcPortPath=\"~/.Cryptomator/ipcPort.bin\"" \
+    --jvm-args "-Xss2m" \
+    --jvm-args "-Xmx512m" \
+    --output app \
+    --force \
+    --identifier org.cryptomator \
+    --name Cryptomator \
+    --version ${BUILD_VERSION} \
+    --module-path ${JAVA_HOME}/jmods\
+    --add-modules java.base,java.logging,java.xml,java.sql,java.management,java.security.sasl,java.naming,java.datatransfer,java.security.jgss,java.rmi,java.scripting,java.prefs,java.desktop,jdk.unsupported \
+    --strip-native-commands
 
 # build AppDir
 mkdir -p Cryptomator.AppDir/usr/share/icons/hicolor/512x512/apps/
 mkdir -p Cryptomator.AppDir/usr/share/icons/hicolor/scalable/apps/
 mkdir -p Cryptomator.AppDir/usr/share/applications/
-cp -r antbuild/Cryptomator/* Cryptomator.AppDir/
+cp -r app/Cryptomator/* Cryptomator.AppDir/
 cp resources/appimage/logback.xml Cryptomator.AppDir/app/
 cp resources/appimage/cryptomator.svg Cryptomator.AppDir/usr/share/icons/hicolor/scalable/apps/
 cp resources/appimage/cryptomator.png Cryptomator.AppDir/usr/share/icons/hicolor/512x512/apps/
 cp resources/appimage/cryptomator.desktop Cryptomator.AppDir/usr/share/applications/
+cp resources/appimage/cryptomator.appdata.xml Cryptomator.AppDir/usr/share/metainfo/
 ln -s usr/share/icons/hicolor/scalable/apps/cryptomator.svg Cryptomator.AppDir/cryptomator.svg
 ln -s usr/share/applications/cryptomator.desktop Cryptomator.AppDir/cryptomator.desktop
 ln -s Cryptomator Cryptomator.AppDir/AppRun
 
-# print resulting AppDir to stdout
-echo "Resulting AppDir:"
-find Cryptomator.AppDir
-echo "--------"
-
-# get and extract appimagetool (extraction needed, as FUSE isn't present on build server)
-curl -L https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage -o appimagetool.AppImage
-chmod +x appimagetool.AppImage
-./appimagetool.AppImage --appimage-extract
+# extract appimagetool (extraction needed, as FUSE isn't present/allowed on build server)
+(cd ./tools/appimagekit && ./appimagetool-x86_64.AppImage --appimage-extract)
 
 # build AppImage
 export ARCH=x86_64
-export PATH=./squashfs-root/usr/bin:${PATH}
 if [[ ${BUILD_VERSION} == "continuous" ]]; then
-  appimagetool Cryptomator.AppDir cryptomator-continuous-x86_64.AppImage -u 'bintray-zsync|cryptomator|cryptomator|cryptomator-linux|cryptomator-continuous-x86_64.AppImage.zsync'
+  ./tools/appimagekit/squashfs-root/AppRun Cryptomator.AppDir cryptomator-continuous-x86_64.AppImage -u 'bintray-zsync|cryptomator|cryptomator|cryptomator-linux|cryptomator-continuous-x86_64.AppImage.zsync'
 else
-  appimagetool Cryptomator.AppDir cryptomator-${BUILD_VERSION}-x86_64.AppImage -u 'bintray-zsync|cryptomator|cryptomator|cryptomator-linux|cryptomator-_latestVersion-x86_64.AppImage.zsync'
+  ./tools/appimagekit/squashfs-root/AppRun Cryptomator.AppDir cryptomator-${BUILD_VERSION}-x86_64.AppImage -u 'bintray-zsync|cryptomator|cryptomator|cryptomator-linux|cryptomator-_latestVersion-x86_64.AppImage.zsync'
 fi
