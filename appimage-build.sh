f6a9b755dd115b41d4a7bde29c89dc8448f79735
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2019-10-16 13:47:55 +0200

    trying to fix build

--- appimage-build.sh
+++ appimage-build.sh
@@ -11,10 +11,10 @@ mkdir -p Cryptomator.AppDir/usr/share/icons/hicolor/512x512/apps/
 mkdir -p Cryptomator.AppDir/usr/share/icons/hicolor/scalable/apps/
 mkdir -p Cryptomator.AppDir/usr/share/applications/
 mkdir -p Cryptomator.AppDir/usr/share/metainfo/
-cp resources/appimage/org.cryptomator.Cryptomator.svg Cryptomator.AppDir/usr/share/icons/hicolor/scalable/apps/
-cp resources/appimage/org.cryptomator.Cryptomator.png Cryptomator.AppDir/usr/share/icons/hicolor/512x512/apps/
-cp resources/appimage/org.cryptomator.Cryptomator.appdata.xml Cryptomator.AppDir/usr/share/metainfo/
-envsubst '${BUILD_VERSION}' < resources/appimage/org.cryptomator.Cryptomator.desktop > Cryptomator.AppDir/usr/share/applications/
+cp resources/appimage/org.cryptomator.Cryptomator.svg Cryptomator.AppDir/usr/share/icons/hicolor/scalable/apps/org.cryptomator.Cryptomator.svg
+cp resources/appimage/org.cryptomator.Cryptomator.png Cryptomator.AppDir/usr/share/icons/hicolor/512x512/apps/org.cryptomator.Cryptomator.png
+cp resources/appimage/org.cryptomator.Cryptomator.appdata.xml Cryptomator.AppDir/usr/share/metainfo/org.cryptomator.Cryptomator.appdata.xml
+envsubst '${BUILD_VERSION}' < resources/appimage/org.cryptomator.Cryptomator.desktop > Cryptomator.AppDir/usr/share/applications/org.cryptomator.Cryptomator.desktop
 ln -s usr/share/icons/hicolor/scalable/apps/org.cryptomator.Cryptomator.svg Cryptomator.AppDir/cryptomator.svg
 ln -s usr/share/icons/hicolor/scalable/apps/org.cryptomator.Cryptomator.svg Cryptomator.AppDir/.DirIcon
 ln -s usr/share/applications/org.cryptomator.Cryptomator.desktop Cryptomator.AppDir/org.cryptomator.Cryptomator.desktop
