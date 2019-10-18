ee457a2d14773c71bd3ee456b6b5bc8f6ac459bb
David Hettler <david-hettler@gmx.de>
2019-10-18 07:48:18 +0200

    Render appdata template instead of hard-coding; add <releases/> section

--- appimage-build.sh
+++ appimage-build.sh
@@ -5,6 +5,9 @@ BUILD_VERSION=${1:-SNAPSHOT}
 if [ ! -d ./Cryptomator ]; then echo "./Cryptomator does not exist."; exit 1; fi
 if [ ! -x $(readlink -e ./tools/appimagekit/squashfs-root/AppRun) ]; then echo "./tools/appimagekit/squashfs-root/AppRun not executable."; exit 1; fi
 
+# render appdata.xml
+./render-appdata-template.sh resources/appdata.template.xml $BUILD_VERSION > resources/appimage/org.cryptomator.Cryptomator.appdata.xml
+
 # prepare AppDir
 mv Cryptomator Cryptomator.AppDir
 mkdir -p Cryptomator.AppDir/usr/share/icons/hicolor/512x512/apps/
