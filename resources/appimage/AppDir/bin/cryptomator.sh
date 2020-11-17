371a49d86db453576d1ebfbaac0d553060fa1a74
Armin Schrenk <armin.schrenk@zoho.eu>
2020-11-17 16:39:27 +0100

    Fix wrong checks in starter script
    * use return code of the GTK check command as indicator that it exists
    * test that return code equals a number and do not run it as a command

--- resources/appimage/AppDir/bin/cryptomator.sh
+++ resources/appimage/AppDir/bin/cryptomator.sh
@@ -5,18 +5,17 @@ cd $(dirname $0)
 GTK2_PRESENT=1 # initially false
 GTK3_PRESENT=0 # initially true
 if command -v dpkg &> /dev/null; then # do stuff for debian based things
-	GTK2_PRESENT=`dpkg -l libgtk* | grep -e '\^ii' | grep -e 'libgtk2-*'`
-	GTK3_PRESENT=`dpkg -l libgtk* | grep -e '\^ii' | grep -e 'libgtk3-*'`
+	GTK2_PRESENT=`dpkg -l libgtk* | grep -e '\^ii' | grep -e 'libgtk2-*' &> /dev/null; echo $?`
+	GTK3_PRESENT=`dpkg -l libgtk* | grep -e '\^ii' | grep -e 'libgtk3-*' &> /dev/null; echo $?`
 elif command -v rpm &> /dev/null; then # do stuff for rpm based things (including yum/dnf)
-	GTK2_PRESENT=`rpm -qa | grep -e '\^gtk2-[0-9][0-9]*'`
-	GTK3_PRESENT=`rpm -qa | grep -e '\^gtk3-[0-9][0-9]*'`
+	GTK2_PRESENT=`rpm -qa | grep -e '\^gtk2-[0-9][0-9]*' &> /dev/null; echo $?`
+	GTK3_PRESENT=`rpm -qa | grep -e '\^gtk3-[0-9][0-9]*' &> /dev/null; echo $?`
 elif command -v pacman &> /dev/null; then # don't forget arch
-	GTK2_PRESENT=`pacman -Qi gtk2`
-	GTK3_PRESENT=`pacman -Qi gtk3`
+	GTK2_PRESENT=`pacman -Qi gtk2 &> /dev/null; echo $?`
+	GTK3_PRESENT=`pacman -Qi gtk3 &> /dev/null; echo $?`
 fi
 
-
-if $GTK2_PRESENT && (! $GTK3_PRESENT ); then
+if [ "$GTK2_PRESENT" -eq 0 ] && [ "$GTK3_PRESENT" -ne 0 ]; then
 	GTK_FLAG="-Djdk.gtk.version=2"
 fi
 
